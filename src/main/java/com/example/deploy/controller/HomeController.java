7d623d9ecd4243f7920cd507bd3bb1e89bba7b18
Nahuel Pierini <nahuelpierini@gmail.com>
2023-10-16 09:52:25 +0200

    first commit

---
+++ src/main/java/com/example/deploy/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String main(){
+        return "Hello World";
+    }
+}
